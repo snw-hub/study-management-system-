2003a51f86b3e9ca71a3bd8715d561f578f2e420
Tshiamo Monageng <90952386+snw-hub@users.noreply.github.com>
2021-09-18 08:21:22 +0200

    Add files via upload

---
+++ Task_1/fetchDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary;
+
+namespace Task_1
+{
+    class fetchDetails
+    {
+        //getters and setters for my variables
+        public string module_code { get; set; }
+        public string module_name { get; set; }
+        public double number_of_credits { get; set; }
+        public double class_hours_per_week { get; set; }
+        public double number_of_weeks { get; set; }
+        public double number_of_hours { get; set; }
+        public DateTime start_date { get; set; }
+        public double study_hours_calculations { get; set; }
+        public double hours_remaining { get; set; }
+    }
+}
